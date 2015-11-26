ebee18de4c607a0141d9bb65e2b741b1536adcd4
asinitsa <asinitsa.consultant@videologygroup.com>
2015-11-26 19:26:32 +0300

    backup and restore

--- solrcloudbck.py
+++ solrcloudbck.py
@@ -8,6 +8,7 @@ import json
 import argparse
 import time
 
+
 class LeaderCore(object):
 
     def __init__(self, collection, shard, shard_range, core_name, node_name, node_port, core_dir):
@@ -35,7 +36,7 @@ class LeaderCore(object):
         return s
 
 
-class CoreListBuilder:
+class SolrCloudBackup:
 
     solr_api_preffix = '/solr/admin/cores?action=STATUS&wt=json&core='
     zk_server_port = '127.0.0.1:2181'
@@ -97,22 +98,100 @@ class CoreListBuilder:
 
         latest_backup_dir = self.backup_dir + '/' + str(max(map(int, os.listdir(self.backup_dir))))
 
-        for collection in os.listdir(latest_backup_dir):
-            for shard_range in os.listdir(latest_backup_dir + '/' + collection):
+        for collection_fs in os.listdir(latest_backup_dir):
+            for shard_range_fs in os.listdir(latest_backup_dir + '/' + collection_fs):
+
+                core_dir_fs = latest_backup_dir + '/' + collection_fs + '/' + shard_range_fs + '/'
 
                 leader_core_obj = LeaderCore(
-                    collection=collection,
-                    shard='shard_fs',
-                    shard_range=shard_range,
-                    core_name='core_name_fs',
-                    node_name='node_name_fs',
-                    node_port='node_port_fs',
-                    core_dir='core_dir_fs'
+                    collection=collection_fs,
+                    shard='empty_field',
+                    shard_range=shard_range_fs,
+                    core_name='empty_field',
+                    node_name='empty_field',
+                    node_port='empty_field',
+                    core_dir=core_dir_fs
                 )
 
                 self.leader_core_list_filesystem.append(leader_core_obj)
 
         return self.leader_core_list_filesystem
+
+    def compare_cores_structures(self, f_cores_list, s_cores_list):
+
+        eq_cores = 0
+        structures_is_equal = False
+
+        f_len = len(f_cores_list)
+        s_len = len(s_cores_list)
+
+        for f_core in f_cores_list:
+            if f_core in s_cores_list:
+                eq_cores += 1
+
+        if f_len != 0 and s_len != 0 and f_len == s_len and eq_cores == s_len:
+            structures_is_equal = True
+        else:
+            print 'ERROR: Different cores number in backup and on server OR different sharding schema.'
+
+        return structures_is_equal
+
+    def backup_from_server_to_filesystem(self):
+
+        s_cores_list = self.get_cores_definitions_server()
+
+        ts = str(int(time.time()))
+        exclude_list = '--exclude "tlog/" --exclude "data/replication.properties" --exclude "core.properties" --exclude "data/index/write.lock" '
+        options_list = '-arvv --delete-before '
+
+        for core in s_cores_list:
+            bck_path = self.backup_dir + '/' + ts + '/' + core.collection + '/' + core.shard_range + '/'
+
+            mkdir_cmd = 'mkdir -p ' + bck_path
+
+            p = os.popen(mkdir_cmd, "r")
+            while 1:
+                line = p.readline()
+                if not line:
+                    break
+                print line
+
+            rsync_cmd = 'rsync ' + options_list + exclude_list + core.node_name + ':' + core.core_dir + ' ' + bck_path
+
+            for i in range(1, 5):  # dirty loop, attempt to be consistent
+                print '-***-'
+                print core.node_name, core.core_dir, 'iteration', i
+                print '-***-'
+                p = os.popen(rsync_cmd, "r")
+                while 1:
+                    line = p.readline()
+                    if not line:
+                        break
+                    print line
+
+        return True
+
+    def restore_from_filesystem_to_server(self):
+
+        f_cores_list = self.get_cores_definitions_filesystem()
+        s_cores_list = self.get_cores_definitions_server()
+
+        restore_possible = self.compare_cores_structures(f_cores_list=f_cores_list, s_cores_list=s_cores_list)
+
+        if restore_possible:
+            print 'Doing restore...'
+
+            for core in s_cores_list:
+                print core.node_name, core.shard_range, core.collection, core.core_dir
+
+            print ''
+
+            for core in f_cores_list:
+                print core.node_name, core.shard_range, core.collection, core.core_dir
+        else:
+            print 'Impossible to restore backup!'
+            return False
+
 
 if __name__ == "__main__":
 
@@ -122,42 +201,6 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    clb = CoreListBuilder(zk_server_port=args.zk_server_port, backup_dir=args.backup_dir)
-    s_cores = clb.get_cores_definitions_server()
-    f_cores = clb.get_cores_definitions_filesystem()
-
-    for s_core in s_cores:
-        for f_core in f_cores:
-            if s_core == f_core:
-                print 'Equal', s_core.shard_range,  f_core.shard_range
-
-    bck_dir = args.backup_dir
-
-    ts = str(int(time.time()))
-    exclude_list = '--exclude "tlog/" --exclude "data/replication.properties" --exclude "core.properties" --exclude "data/index/write.lock" '
-
-    for core in s_cores:
-        bck_path = bck_dir + '/' + ts + '/' + core.collection + '/' + core.shard_range + '/'
-
-        mkdir_cmd = 'mkdir -p ' + bck_path
-
-        p = os.popen(mkdir_cmd, "r")
-        while 1:
-            line = p.readline()
-            if not line:
-                break
-            print line
-
-        rsync_cmd = 'rsync -arvv --delete-before ' + exclude_list + core.node_name + ':' + core.core_dir + ' ' + bck_path
-        #rsync_cmd = "exit 0"
-
-        for i in range(1, 5):  # dirty loop
-            print '-***-'
-            print core.node_name, core.core_dir, 'iteration', i
-            print '-***-'
-            p = os.popen(rsync_cmd, "r")
-            while 1:
-                line = p.readline()
-                if not line:
-                    break
-                print line
+    clb = SolrCloudBackup(zk_server_port=args.zk_server_port, backup_dir=args.backup_dir)
+    #r = clb.backup_from_server_to_filesystem()
+    r = clb.restore_from_filesystem_to_server()
